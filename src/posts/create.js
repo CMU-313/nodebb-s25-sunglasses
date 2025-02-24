b34bda1e02792a9a9f2e615b595f25c97d235943
Kayo Kuri <kayokuri@kayos-mbp.wifi.local.cmu.edu>
2025-02-24 15:35:41 -0500

    fix: translation features passes lint

--- src/posts/create.js
+++ src/posts/create.js
@@ -20,7 +20,7 @@ module.exports = function (Posts) {
 		const content = data.content.toString();
 		const timestamp = data.timestamp || Date.now();
 		const isMain = data.isMain || false;
-		const [english, translatedTxt] = await translationApi.translate(data);		
+		const [english, translatedTxt] = await translationApi.translate(data);
 
 		if (!uid && parseInt(uid, 10) !== 0) {
 			throw new Error('[[error:invalid-uid]]');
