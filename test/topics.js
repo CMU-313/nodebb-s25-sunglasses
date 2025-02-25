b951b6069e316c8d43a8da4d09161fb8b13c6135
Emily Xie <30580023+emiilyxie@users.noreply.github.com>
2025-02-25 15:48:14 -0500

    test: add tests for anonymous replies

--- test/topics.js
+++ test/topics.js
@@ -279,6 +279,30 @@ describe('Topic\'s', () => {
 			assert.equal(postData[0].pid, result.pid, 'result should be the reply we added');
 		});
 
+		it('should create not anonymous replies', async () => {
+			const result = await topics.reply({ uid: topic.userId, content: 'test not anonymous reply', tid: newTopic.tid, toPid: newPost.pid, anonymous: false });
+			assert.ok(result);
+
+			const postData = await apiPosts.getReplies({ uid: 0 }, { pid: newPost.pid });
+			assert.ok(postData);
+
+			const anonymousReply = postData.find(reply => reply.pid === result.pid);
+			assert.ok(anonymousReply, 'the not anonymous reply should be present');
+			assert.equal(anonymousReply.anonymous, 0, 'should not be anonymous');
+		});
+
+		it('should create anonymous replies', async () => {
+			const result = await topics.reply({ uid: topic.userId, content: 'test anonymous reply', tid: newTopic.tid, toPid: newPost.pid, anonymous: true });
+			assert.ok(result);
+
+			const postData = await apiPosts.getReplies({ uid: 0 }, { pid: newPost.pid });
+			assert.ok(postData);
+
+			const anonymousReply = postData.find(reply => reply.pid === result.pid);
+			assert.ok(anonymousReply, 'the anonymous reply should be present');
+			assert.equal(anonymousReply.anonymous, 1, 'should be anonymous');
+		});
+
 		it('should error if pid is not a number', async () => {
 			await assert.rejects(
 				apiPosts.getReplies({ uid: 0 }, { pid: 'abc' }),
